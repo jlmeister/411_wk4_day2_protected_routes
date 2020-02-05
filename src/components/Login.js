d25ec14f6481e567c6a6447d19c13beb513f5098
jlmeister <iamphil4483@yahoo.com>
2020-02-04 22:11:26 -0600

    created protected routes, mock authorization, and authorized routing for react app.

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,10 +20,8 @@ class App extends Component {
 
   login = (e) => {
     e.preventDefault()
-    // set cookie here
-    // set loggedIn = true and max-age = 60*1000 (one minute)
-
-    window.location.replace("/")
+    document.cookie = 'loggedIn=true;max-age=60';
+    this.props.history.push('/')
   }
 
   render() {
